ec160251ac11dd96dedb0b4eeab236b0a691be4d
Marc Ucman <marcucman@yahoo.com>
2017-03-11 02:08:10 -0600

    prep repo for heroku

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ var express = require('express'); // now the express api is available
 
 // create the app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+// express middleware
+app.use(function(req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 // express.static('folderName') tells express where to look for files to serve up
 app.use(express.static('public'));
 
 // start server
 // app.listen(port, function to call when server is up)
-app.listen(3000, function() {
-  console.log('Express server is up on port 3000');
+app.listen(port, function() {
+  console.log('Express server is up on port ' + PORT);
 });
