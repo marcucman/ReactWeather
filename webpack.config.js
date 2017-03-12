6573ad637b5387475299aa3e8f06ee9c86e37e90
Marc Ucman <marcucman@yahoo.com>
2017-03-11 18:36:16 -0600

    Add Foundation and jQuery

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,20 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './app/app.jsx', // where to pull the app code from
+  entry: [
+    'script!jquery/dist/jquery.min.js', // use script-loader npm module
+    'script!foundation-sites/dist/foundation.min.js', // use script-loader npm module
+    './app/app.jsx'
+  ], // where to pull the app code from
+  externals: { // key = module name, value = variable name we want available in external script files
+    jquery: 'jQuery' // allows foundation properly attach methods to jQuery object
+  },
+  plugins: [
+    new webpack.ProvidePlugin({ // key = variable name to watch for, value = module to replace with
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
   output: {
     path: __dirname, // gives the path to the current folder (the root directoy)
     filename: './public/bundle.js' // where to compile the app code at
