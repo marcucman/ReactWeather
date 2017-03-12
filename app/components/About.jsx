1dd0acb19b13c9e299152a7c756ea1c9b03bc266
Marc Ucman <marcucman@yahoo.com>
2017-03-11 19:22:04 -0600

    style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -17,10 +17,29 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About Page</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application built with React and Foundation.
+        Enter a city name and get the temperature for that city.</p>
+      <p>Here are some of the tools used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - This was used to return weather data
+        </li>
+      </ul>
     </div>
   )
 };
+
+// var About = (props) => { // old About without Foundation
+//   return (
+//     <div>
+//       <h3>About</h3>
+//       <p>Welcome to the About Page</p>
+//     </div>
+//   )
+// };
 
 module.exports = About;
